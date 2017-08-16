721c60570a57d2fe2c735a79fee9f0d8a8e00481
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-16 14:37:39 -0400

    first commit, added and required packages

---
+++ app.js
@@ -0,0 +1,11 @@
+//===Packages===//
+
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const flash = require('express-flash-messages');
+const session = require('express-session');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const bp = require('body-parser')
