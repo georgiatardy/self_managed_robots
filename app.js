808bd1e57c1fdc0b36c384b085260eb251e8d856
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-16 21:43:50 -0400

    completed app.js file, no errors, app is running

--- app.js
+++ app.js
@@ -1,11 +1,134 @@
 //===Packages===//
 
+const fs = require('fs');
+const robots = require('./routes/robots');
+const db = require('./db');
+// const LocalStrategy = require('passport-local').Strategy
+
 const express = require('express');
-const bcryptjs = require('bcryptjs');
+// const bcryptjs = require('bcryptjs');
 const flash = require('express-flash-messages');
 const session = require('express-session');
-const mongodb = require('mongodb');
-const mongoose = require('mongoose');
+// const mongodb = require('mongodb');
+// const mongoose = require('mongoose');
 const passport = require('passport');
-const passportLocal = require('passport-local');
-const bp = require('body-parser')
+const passportLocal = require('passport-local').Strategy
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+let url = 'mongodb://localhost:27017/robotDatabase';
+const app = express();
+// const robots = ('./routes/robots');
+
+
+// ================= Boilerplate ================ //
+
+// === Passport === //
+passport.use(
+  new passportLocal(function(username, password, done) {
+    console.log('passportLocal', username, password);
+    Robot.authenticate(username, password)
+      // === Success!! === //
+      .then(user => {
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, null, {
+            message: 'There was no user with this email and password.'
+          });
+        }
+      })
+      // === There was a problem === //
+      .catch(err => done(err));
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+// === Middleware to parse form data === //
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// === For Handlebars Express === //
+
+app.engine('handlebars', handlebars());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// === Creates A Session === //
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUnintialized: false,
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(flash());
+
+// === Local Login Form === //
+
+app.get('/login', (req, res) => {
+  res.render('loginForm', { failed: req.query.failed });
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login?failed=true',
+    failureFlash: true
+  })
+);
+
+app.get('/register', (req, res) => {
+  res.render('registerForm');
+});
+
+app.post('/register', (req, res) => {
+  let robot = new Robot(req.body);
+  robot.provider = 'local';
+  robot.setPassword(req.body.password);
+
+  robot
+    .save()
+    // if good...
+    .then(() => res.redirect('/'))
+    // if bad...
+    .catch(err => console.log(err));
+});
+
+// === Log out === //
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// ========Connection to Robot Database==== //
+
+
+app.use('/', robots);
+
+
+db.connect(url, (err, connection) => {
+  if (!err)
+    console.log('connected to Mongo.');
+
+
+  app.listen(3000, function() {
+    console.log('App is running!');
+  });
+})
---
+++ routes/robots.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = express.Router();
+const db = require('../db');
+
+
+
+// routes.use();
+
+routes.get('/', (req, res) => {
+  let collect = db.get().collection('robot');
+
+  collect.find({}).toArray((err, robot) => {
+    res.render('home', {robots: robot}); console.log({robots:robot});
+
+  });
+});
+
+//===Profile page for individual robots===//
+
+routes.get('robot/:robot', (req, res) => {
+  let robot = req.params.robot
+  let collection = db.get().collection('robot');
+
+  collection.find({robots: robot}).toArray ((err, robot) => {
+    console.log(robot);
+    res.render('profile', {robots: robot});
+  });
+});
+
+module.exports = routes;
